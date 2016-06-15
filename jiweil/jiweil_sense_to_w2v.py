3ca031a5b7c195fafd9000874d34bec928171df8
Márton Makrai <makrai.nlp@gmail.com>
2016-06-15 15:44:23 +0200

    adjusted to the present format, old format raises NotImplementedError

--- jiweil/jiweil_sense_to_w2v.py
+++ jiweil/jiweil_sense_to_w2v.py
@@ -14,11 +14,18 @@ def jiweil_posproc(args):
             vals = embed_f.readline().strip().split()
             if vals:
                 len_vals = len(vals)
-                if len_vals in [4,2]:
-                    #4: "word", word_i, sense_i, prob
-                    word_i = int(vals[1])
-                elif len_vals == 1:
-                    # line is like "sense0"
+                if len_vals == 4:
+                    raise NotImplementedError(
+                    'the version that wrties lines of the form `"word" word_i\
+                    sense_i prob` is not handled')
+                elif len_vals in [1, 2]:
+                    if vals[0] == 'word':
+                    # line is like
+                    # len_vals == 2 "word 0" or 
+                        word_i = int(vals[1])
+                    else:
+                        # line is like "sense0" or "sense0 0.8816724201743471"
+                        pass
                     continue
                 else:
                     print '{} {}'.format(vocab[word_i], ' '.join(vals[1:]))
